ee8d56cfcaaa9c9c358e90a60362860a04ea5848
mtafadar <>
2022-09-05 04:33:26 +0000

    trying to make functinality 8 to work

--- main.py
+++ main.py
@@ -226,6 +226,7 @@ def commandSixFunction(dbConn):
         plt.xlabel("month")
         plt.ylabel("number of riders (x*10^8")
         plt.title("monthly ridership")
+        plt.ion()
         plt.plot(x,y)
         plt.show()
 
@@ -255,6 +256,7 @@ def commandSevenFunction(dbConn):
         plt.xlabel("year")
         plt.ylabel("number of riders (x*10^8")
         plt.title("yearly ridership")
+        plt.ion()
         plt.plot(x,y)
         plt.show()
   
@@ -280,14 +282,113 @@ def  CheckforNumberOfStation(dbConn, Val):
        return True;
        
      return;
-      
-        
-      
+
+
+def  CommandEightFunction(dbConn, year, firstStationName, secondStationName):
+  dbcusorStation = dbConn.cursor(); 
+  dbcusorStationNameByDate = dbConn.cursor(); 
+
+  
+  sqlforGenerictStation = """Select Station_ID, Station_Name From Stations 
+  Where Station_Name  like  ? """
+
+  
+  
+  sqlforMainQueryStationOne = """SELECT 
+  Date(R.Ride_Date), 
+   R.Num_Riders
+FROM 
+(
+  SELECT
+    Ride_Date,
+    sum(Num_Riders) as Num_Riders,
+    ROW_NUMBER() OVER (ORDER BY Ride_Date  ASC) AS rn_up,
+    ROW_NUMBER() OVER (ORDER BY Ride_Date DESC) AS rn_down
+  FROM Ridership
+  inner join Stations as s  on s.Station_ID = Ridership.Station_ID
+  Where strftime('%Y', Ride_Date) = ? AND s.Station_Name like ?
+  group by Ride_Date
+) AS R
+WHERE R.rn_up <= 5 OR R.rn_down <= 5
+ORDER BY R.Ride_Date ASC;"""
+
+  SqlforGraph = """Select Date(Ride_Date) , sum(Num_Riders) from Ridership  inner join  Stations  on Stations.Station_ID = Ridership.Station_ID Where strftime('%Y', Ride_Date) = ? AND Station_Name like ? Group By  Ride_Date""";
+
+
+
   
 
+
+
+
+
+  
+  dbcusorStation.execute(sqlforGenerictStation, [firstStationName])
+  fetchStation =  dbcusorStation.fetchall();
+
+  dbcusorStationNameByDate.execute(sqlforMainQueryStationOne, [year, firstStationName])
+  fetchFirstStation = dbcusorStationNameByDate.fetchall();
+
+  dbcusorStation.execute(sqlforGenerictStation, [secondStationName])
+  fetchStation2 = dbcusorStation.fetchall();
+
+  dbcusorStationNameByDate.execute(sqlforMainQueryStationOne, [year,secondStationName])
+  fetchFirstStation2 = dbcusorStationNameByDate.fetchall();
+
+
+  dbcusorStationNameByDate.execute(SqlforGraph , [year, firstStationName])
+  fetchForPlot = dbcusorStationNameByDate.fetchall(); 
+
+
+
+  for row in fetchStation:
+     print("Station 1: ", row[0], row[1])
+    
+  
+  for row in fetchFirstStation: 
+    print( row[0],  row[1])
+
+  for row in fetchStation2:
+    print("Station 2: ", row[0], row[1])
+
+  for row in fetchFirstStation2: 
+    print( row[0],  row[1])
+
+
+  
      
+  
+    
+
+  plotInput = input("plot: (y/n)")
+  if (plotInput == 'y'):
+    x1= []
+    y1 = []
+    x2 = []
+    y2 = []
+    day  = 1;
+  
+    for row in fetchForPlot:
+      x1.append(day)
+      y1.append(row[1])
+      day = day + 1;
+      
 
     
+    
+
+    
+
+    
+
+
+    # plt.xlabel("year")
+    # plt.ylabel("number of riders (x*10^8")
+    # plt.title("yearly ridership")
+     plt.ion()
+    plt.plot(x1,y1)
+    plt.show()
+  
   
     
           
@@ -335,24 +436,20 @@ while (True):
            commandSevenFunction(dbConn);
 
         if(int(GenericInputVal) == 8):
-          commandEightInput  = int(input( "Year to compare against?"));
+          commandInputYear  = (input( "Year to compare against?"));
           commandStationNameInput = input("Enter station 1 (wildcards _ and %):");
           if(CheckforNumberOfStation(dbConn, commandStationNameInput) == True):
             commandStationNameInput2 = input("Enter station (wildcards _ and %):");
             if(CheckforNumberOfStation(dbConn, commandStationNameInput2) == True):
-               print("plotting here")
+              CommandEightFunction(dbConn, commandInputYear , commandStationNameInput,commandStationNameInput2)
               
-            
-          
-          
-          
 
-          
-          
-                
+              
+                 
 
+  
     else:
-        print("**Error, unknown command, try again...")
+       print("**Error, unknown command, try again...")
 
 #
 # done
